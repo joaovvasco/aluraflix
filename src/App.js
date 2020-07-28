a94f85bd7ef89f1e8e7c4b525def2864415577d8
João Victor <jovicto1990@gmail.com>
2020-07-28 00:53:06 -0300

    Ajuste final de adaptação

--- src/App.js
+++ src/App.js
@@ -21,25 +21,6 @@ function App() {
         category={dadosIniciais.categorias[0]}
       />
 
-      <Carousel
-        category={dadosIniciais.categorias[1]}
-      />
-
-      <Carousel
-        category={dadosIniciais.categorias[2]}
-      />      
-
-      <Carousel
-        category={dadosIniciais.categorias[3]}
-      />      
-
-      <Carousel
-        category={dadosIniciais.categorias[4]}
-      />      
-
-      <Carousel
-        category={dadosIniciais.categorias[5]}
-      />      
 
       <Footer />
     </div>
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { FooterBase } from './styles';
+import LogoFacebook from '../../assets/img/logo_facebook.png';
+import LogoInstagram from '../../assets/img/logo_insta.png';
 
 function Footer() {
+  const baseUrl = `${__dirname}/assets/img`;
+
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      <a href="https://www.instagram.com/ospowerrangersbelem/?hl=pt-br">
+        <img className="Logo" src={LogoInstagram} alt="AluraFlix logo" />
       </a>
       <p>
         Orgulhosamente criado durante a
